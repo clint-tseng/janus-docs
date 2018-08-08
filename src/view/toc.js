05a2d32505b3911b70349e8a3ed4cd4195979e69
Clint Tseng <clint@dontexplain.com>
2018-08-08 15:13:31 -0700

    new: hide/show toc trees on activation.

--- src/view/toc.js
+++ src/view/toc.js
@@ -5,7 +5,12 @@ const { Toc } = require('../model/toc');
 const TocViewModel = Model.build(
   bind('active', from('subject').watch('path')
     .and.app('path').map((path) => path.replace(/^(.+)\/$/, '\1'))
-    .all.map((own, current) => own === current))
+    .all.map((own, current) => own === current)),
+
+  bind('prefix', from('subject').watch('path')
+    .and.app('path')
+    .all.map((own, current) => current.startsWith(own) ||
+      ((current === '/') && (own === '/intro')))) // special case: show getting started on homepage
 );
 
 const TocView = DomView.build($(`
@@ -14,7 +19,9 @@ const TocView = DomView.build($(`
     <div class="toc-children"/>
   </div>`), template(
 
-  find('.toc-entry').classed('active', from('active')),
+  find('.toc-entry')
+    .classed('active', from('active'))
+    .classed('prefix', from('prefix')),
 
   find('a')
     .text(from('subject').watch('title'))
