292c7e9af7eced79f3a47b5decf759abc578d4f2
Clint Tseng <clint@dontexplain.com>
2019-03-26 00:40:51 -0700

    inspect/new: make listview inspection work as expected.

--- src/util/inspect.js
+++ src/util/inspect.js
@@ -5,6 +5,7 @@ const { asPanel } = require('../view/context');
 // we make our own inspect() that prevents re-inspection.
 const inspect = (x) => {
   // this is sort of a cheat; if we inspect a View without an app, splice one in.
+  // TODO: remove
   if ((x != null) && (x instanceof DomView) && (x.options.app == null))
     x.options.app = app;
 
@@ -14,6 +15,16 @@ const inspect = (x) => {
 
 // augment inspect with the ability to directly request a panel view.
 inspect.panel = (x) => asPanel(inspect(x));
+
+
+// TODO: for now, we put some stdlib mutator extractors here but it's not clear
+// where they really belong. it's obviously not here. it's also not clear how the
+// library should be populated but we should obviously not be doing it globally.
+const { ListView } = require('janus-stdlib').view.list;
+const { Mutation, DomViewInspector } = jInspect.inspector.domview;
+const listViewExtractor = (view) => view._mappedBindings.mapPairs((idx, binding) =>
+  new Mutation({ operation: `[${idx}]`, binding: binding.parent }));
+DomViewInspector.extractors.register(ListView, listViewExtractor);
 
 module.exports = { inspect };
 
