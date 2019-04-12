01773d872f30cdd615c4f742be9840a0c8c0f3ec
Clint Tseng <clint@dontexplain.com>
2019-04-12 00:17:15 -0700

    flyout/new: add manual flyout implementation.

--- src/model/flyout.js
+++ src/model/flyout.js
@@ -1,12 +1,34 @@
 const { Model, attribute, bind, from, List } = require('janus');
 const { sticky, fromEvents } = require('janus-stdlib').varying;
 
+// TODO: once Model.build() x2 is unbroken for ES6 (see janus#150) we can have the
+// two Flyout types inherit from a common ancestor to solve some typing issues
+// elsewhere and add methods like addChild(flyout) without hating it.
+
+// there are two flyout classes. manual flyouts only close when a button is
+// clicked. hover flyouts, which are more commonly used, close when the mouse
+// has left areas of interest (the original trigger, the flyout, and its
+// descendants) for a small length of time.
+
 // add ourselves to our trigger's flyout parent to prevent its destruction.
 const addToParent = (flyout) => {
   const parentDom = flyout.get_('trigger').closest('.flyout');
   if (parentDom.length > 0)
     parentDom.view().subject.get_('children').add(flyout);
 };
+
+
+// manual flyouts don't have much to do.
+class ManualFlyout extends Model.build(
+  attribute('children', attribute.List.withDefault())
+) {
+  _initialize() {
+    this.destroyWith(this.get_('target'));
+    addToParent(this);
+  }
+}
+
+// hover flyouts have to track hovers.
 class HoverFlyout extends Model.build(
   attribute('children', attribute.List.withDefault()),
 
@@ -34,6 +56,8 @@ class HoverFlyout extends Model.build(
 
 module.exports = {
   Flyout: {
+    manual: ManualFlyout,
+    Manual: ManualFlyout,
     hover: HoverFlyout,
     Hover: HoverFlyout
   }
--- src/view/flyout.js
+++ src/view/flyout.js
@@ -1,11 +1,32 @@
-const { DomView, find, from } = require('janus');
+const { DomView, template, find, from } = require('janus');
 const $ = require('janus-dollar');
 
 const { Flyout } = require('../model/flyout');
 const { positionFlyout } = require('../util/dom.js');
 
 
-class FlyoutView extends DomView.build(
+// TODO: because the close button is part of the enclosing container, it seems
+// like it'll be really difficult to style for different contents. but also,
+// requiring contents to supply their own close buttons seems bad too.
+class ManualFlyoutView extends DomView.build($(`
+  <div class="flyout">
+    <button class="flyout-close" title="Close"/>
+    <div class="flyout-contents"/>
+  </div>`), template(
+  find('.flyout-contents').render(from('target')).context(from('context')),
+  find('.flyout-close').on('click', (e, subject) => { subject.destroy(); })
+)) {
+  _wireEvents() {
+    // we do this here instead of _render because here we are guaranteed to be
+    // attached to the document, which we need in order to measure ourselves.
+    positionFlyout(this.subject.get_('trigger'), this.artifact());
+    this.destroyWith(this.subject.get_('target'));
+    this.destroyWith(this.subject);
+    this.subject.destroyWith(this); // TODO: some sort of mutual pact shortcut?
+  }
+}
+
+class HoverFlyoutView extends DomView.build(
   $('<div class="flyout"/>'),
   find('.flyout')
     .render(from('target')).context(from('context'))
@@ -13,14 +34,16 @@ class FlyoutView extends DomView.build(
     .on('mouseleave', (_, subject) => { subject.set('hover.target', false); })
 ) {
   _wireEvents() {
-    // we do this here instead of _render because here we are guaranteed to be
-    // attached to the document, which we need in order to measure ourselves.
-    positionFlyout(this.subject.get_('trigger'), this.artifact());
+    ManualFlyoutView.prototype._wireEvents.call(this);
   }
 }
 
+
 module.exports = {
-  FlyoutView,
-  registerWith: (library) => { library.register(Flyout, FlyoutView) }
+  ManualFlyoutView, HoverFlyoutView,
+  registerWith: (library) => {
+    library.register(Flyout.Manual, ManualFlyoutView);
+    library.register(Flyout.Hover, HoverFlyoutView);
+  }
 };
 
