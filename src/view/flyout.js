0b5ca882d81635245293efcbdc93cd0432a09ec5
Clint Tseng <clint@dontexplain.com>
2019-05-09 17:14:31 -0700

    repl/bug: make flyout-exceptions closeable.

--- src/view/flyout.js
+++ src/view/flyout.js
@@ -5,14 +5,21 @@ const { Flyout } = require('../model/flyout');
 const { positionFlyout } = require('../util/dom.js');
 
 
+const className = (x) => {
+  if (x == null) return null;
+  else if (x.constructor == null) return null;
+  else return x.constructor.name.replace(/^.+Error/, 'Error');
+};
+
 // TODO: because the close button is part of the enclosing container, it seems
 // like it'll be really difficult to style for different contents. but also,
 // requiring contents to supply their own close buttons seems bad too.
 class ManualFlyoutView extends DomView.build($(`
   <div class="flyout">
-    <button class="flyout-close" title="Close"/>
     <div class="flyout-contents"/>
+    <button class="flyout-close" title="Close"/>
   </div>`), template(
+  find('.flyout').classGroup('class-', from('target').map(className)),
   find('.flyout-contents').render(from('target')).context(from('context')),
   find('.flyout-close').on('click', (e, subject) => { subject.destroy(); })
 )) {
