4cf5dd299e1be1710e7e2da8f31d7c36cb25bd96
Issa Tseng <maintainer@heyissa.com>
2019-12-10 18:10:56 -0800

    new: show page titles.
    * a hack, but the articles themselves don't have titles on them yet.

--- src/view/toc.js
+++ src/view/toc.js
@@ -2,16 +2,23 @@ const { DomView, template, find, from, Model, bind } = require('janus');
 const { Toc } = require('../model/toc');
 const { ApiBrowser } = require('./api');
 
-const TocViewModel = Model.build(
+class TocViewModel extends Model.build(
   bind('active', from.subject('path')
-    .and.app('path').map((path) => path.replace(/^(.+)\/$/, '\1'))
+    .and.app('path').map((path) => path.replace(/^(.+)\/$/, '\\1'))
     .all.map((own, current) => own === current)),
 
   bind('prefix', from.subject('path')
     .and.app('path')
     .all.map((own, current) => current.startsWith(own) ||
       ((current === '/') && (own === '/intro')))) // special case: show getting started on homepage
-);
+) {
+  _initialize() {
+    // TODO: sort of hackish but this information is sort of scattered around.
+    this.reactTo(this.get('active'), (active) => {
+      if (active) $('title').text(`Janus | ${this.get_('subject').get_('title')}`);
+    });
+  }
+};
 
 const TocView = DomView.build(TocViewModel, $(`
   <div class="toc-entry">
