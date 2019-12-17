13d22bfc0b05e1ce324810bf36c9fd5e976eed60
Issa Tseng <maintainer@heyissa.com>
2019-12-17 15:17:57 -0800

    bug: don't steal away a dom node that's already on the page.
    * eventually we should probably display something a little more
      intelligent than just (dom node).

--- src/view/jquery.js
+++ src/view/jquery.js
@@ -1,7 +1,10 @@
 const { DomView } = require('janus');
 
 class JQueryView extends DomView {
-  _render() { return this.subject; }
+  _render() {
+    if (this.subject.parent().length > 0) return $('<span>(dom node)</span>');
+    return this.subject;
+  }
 }
 
 module.exports = {
